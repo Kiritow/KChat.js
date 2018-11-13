5d61c197369623a5a4271e90243cec566e979b08
Kiritow <1362050620@qq.com>
2018-11-13 18:40:34 +0800

    后端基础服务

--- kchat.js
+++ kchat.js
@@ -45,18 +45,119 @@ let server=new WebSocketServer({
     autoAcceptConnections: false
 })
 
-server.on('request',(request)=>{
-    let connection=request.accept('kchat-v1',request.origin)
-    console.log('Connection accepted')
-    connection.on('message',(message)=>{
-        if(message.type=='utf8') {
-            console.log('Receive message: '+message.utf8Data)
-            connection.sendUTF(message.utf8Data)
-        } else {
-            console.log('get binary data')
+// Array? emmm... may have performance issues.
+let clients=new Array()
+
+function RemoveClientByConn(conn) {
+    for(let i=0;i<clients.length;i++) {
+        if(clients[i].conn==conn) {
+            console.log("Connection found in list. Removing...")
+            clients[i].close()
+            clients.splice(i,i) // Is this ok?
+            return true
         }
-    })
-    connection.on('close',(reasonCode,desc)=>{
-        console.log('ws client close ' + reasonCode + ' '+ desc)
-    })
+    }
+    console.log("Connection not in list. ")
+    return false
+}
+
+function GetClientByConn(conn) {
+    for(let i=0;i<clients.length;i++) {
+        if(clients[i].conn==conn) {
+            return clients[i]
+        }
+    }
+    return null
+}
+
+function AddNewClient(conn) {
+    return clients[clients.push({
+        conn:conn,
+        nickname:null,
+        handshake_done:false
+    })-1]
+}
+
+function SendToAll(obj) {
+    for(let i=0;i<clients.length;i++){
+        clients[i].conn.sendUTF(JSON.stringify(obj))
+    }
+}
+
+function SendToOthers(except_conn,obj) {
+    for(let i=0;i<clients.length;i++){
+        if(clients[i].conn!=except_conn) clients[i].conn.sendUTF(JSON.stringify(obj))
+    }
+}
+
+function SendToOne(this_conn,obj) {
+    this_conn.sendUTF(JSON.stringify(obj))
+}
+
+server.on('request',(request)=>{
+    try {
+        let connection=request.accept('kchat-v1',request.origin)
+        console.log('Websocket connection accepted.')
+        let thisClient=AddNewClient(connection)
+
+        connection.on('message',(message)=>{
+            if(message.type=='utf8') {
+                let j=JSON.parse(message.utf8Data)
+
+                if(j.type=='handshake') {
+                    if(thisClient.handshake_done) {
+                        console.warn("Double handshake. Something is wrong")
+                        SendToOne(connection,{type:"message",isSysMsg:true,message:"您已登录. 请勿重新登录"})
+                    } else {
+                        if(j.newuser) {
+                            SendToAll({type:"message",isSysMsg:true,message:"欢迎新人~!"})
+                        } else {
+                            thisClient.nickname=j.nickname
+                            thisClient.handshake_done=true
+                            SendToOne(connection,{type:"message",isSysMsg:true,message:`欢迎回来, ${thisClient.nickname}`})
+                            SendToOthers(connection,{type:"message",isSysMsg:true,message:`${thisClient.nickname} 加入了聊天室.`})
+                        }
+                    }
+                } else if(j.type=='operation') {
+                    if(!thisClient.handshake_done) {
+                        console.warn("Invalid user. Something is wrong")
+                        SendToOne(connection,{type:"message",isSysMsg:true,message:"操作失败. 请先登录."})
+                    } else {
+                        if(j.operation=="nickname_change") {
+                            if(thisClient.nickname==null) {
+                                SendToAll({type:"message",isSysMsg:true,message:`${thisClient.nickname} 加入了聊天室.`})
+                            } else {
+                                let oldname=thisClient.nickname
+                                let newname=j.newname
+                                thisClient.nickname=newname
+                                SendToAll({type:"message",isSysMsg:true,message:`${oldname} 修改了昵称为 ${newname}`})
+                            }
+                        } else {
+                            console.warn("Unknown operation: " + j.operation)
+                        }
+                    }
+                } else if(j.type=='message') {
+                    SendToAll({type:"message",isSysMsg:false,sender:thisClient.nickname,message:j.message})
+                } else {
+                    console.warn("Unknown type: " + j.type)
+                }
+            } else {
+                // Currently, binary data is not supported.
+                // If a client send binary data, it will be kicked from chatroom.
+                console.log('get binary data. kicking this client...')
+                let nickname=thisClient.nickname
+                RemoveClientByConn(connection)
+                SendToAll({type:"message",isSysMsg:true,message:`${nickname} 被移出聊天室.`})
+            }
+        })
+
+        connection.on('close',(reasonCode,desc)=>{
+            console.log('ws client close ' + reasonCode + ' '+ desc)
+            let nickname=thisClient.nickname
+            RemoveClientByConn(connection)
+            SendToAll({type:"message",isSysMsg:true,message:`${nickname} 离开了.`})
+        })
+    } catch (e) {
+        console.warn("Exception: " + e)
+    }
 })
